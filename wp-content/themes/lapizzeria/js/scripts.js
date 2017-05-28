6b7870e21c7347011000130a037d61ae4c95156d
frankweb2 <frank.morales.ing.informatica@gmail.com>
2017-05-27 23:04:11 -0400

    sesubió archivo scripts.js

--- wp-content/themes/lapizzeria/js/scripts.js
+++ wp-content/themes/lapizzeria/js/scripts.js
@@ -1,4 +1,19 @@
 $ = jQuery.noConflict();
 $(document).ready(function(){
-  alert('hola');
+
+  //ocultar y mostrar
+  $('.mobile-menu a').on('click', function(){
+    $('nav.menu-sitio').toggle('show');
+  });
+
+///reacción a resize en la pantalla
+  var breakpoint = 768;
+
+  $(window).resize(function(){
+    if($(document).width() >= breakpoint){
+      $('nav.menu-sitio').show();
+    }else{
+      $('nav.menu-sitio').hide();
+    }
+  });
 });
